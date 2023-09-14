c7a7f98e0995e815c9739b8ddf993d25d0a48ab0
anaika <102043114+anaikaaaa@users.noreply.github.com>
2023-09-14 21:22:42 +0000

    Update findOneByEmail.ts

--- src/findOneByEmail.ts
+++ src/findOneByEmail.ts
@@ -1,8 +1,10 @@
 import { Contacto, contactos } from "./contactos";
 
-function findOneByEmail(parteDeUnCorreo:string) : Contacto {
+function findOneByEmail(unContacto: string): Contacto {
+  const encontrados = contactos.filter((contacto) =>
+    contacto.email.includes(unContacto),
+  );
+  return encontrados[0];
+}
 
-    const encontrados = contactos.filter(contacto=> contacto.email.includes(parteDeUnCorreo))
-     return encontrados[0];
-}  
- export {findOneByEmail}
+export { findOneByEmail };
